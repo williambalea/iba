b501f7c210161aea952d1dc8258992780137b22e
William Balea <william.balea@polymtl.ca>
2022-02-03 18:13:09 -0500

    remade hero

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -6,8 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accueil.component.scss']
 })
 export class AccueilComponent implements OnInit {
+  heroImage: string;
 
-  constructor() { }
+  images: string[] = [
+    "cuisine",
+    "cuisine2",
+    "ilot",
+    "salon",
+    "salon2",
+    "sdb"
+  ]
+
+  constructor() {
+    const index: number = Math.floor(Math.random() * this.images.length);
+    const image: string = this.images[index];
+    console.log(image)
+    this.heroImage = `url('../../assets/heroImages/${image}.jpg')`
+  }
 
   ngOnInit(): void {
   }
