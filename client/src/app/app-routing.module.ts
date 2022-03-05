6a2d48abf001d53148a082ab4064ea6b9ecbaa90
williambalea <williambalea27@gmail.com>
2022-03-04 21:46:01 -0500

    added rejoindre route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RejoindreComponent } from './rejoindre/rejoindre.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/accueil', pathMatch: 'full'},
+  { path: '', component: AccueilComponent },
+  { path: 'rejoindre', component: RejoindreComponent },
+  { path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccueilComponent } from './accueil/accueil.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AccueilComponent } from './accueil/accueil.component';
+import { RejoindreComponent } from './rejoindre/rejoindre.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import { RejoindreComponent } from './rejoindre/rejoindre.component';
+
 import { KitchenSvgComponent } from '../assets/svg/kitchen/kitchen.component';
 import { doorSvgComponent } from '../assets/svg/door/door.component';
 import { sinkSvgComponent } from '../assets/svg/sink/sink.component';
